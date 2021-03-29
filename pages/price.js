cf17c8c2063485f59cf7a81d1932db6622945f92
Matan Nahmani <matanmatan999new@gmail.com>
2021-03-29 11:10:35 +0300

    new tabs style

--- pages/price.js
+++ pages/price.js
@@ -1,19 +1,18 @@
-import { Grid, Spacer, Text } from "@geist-ui/react";
-import Tabs from "../components/Tabs";
+import { Grid, Spacer, Text, Tabs } from "@geist-ui/react";
 
 const Pricing = () => {
 
     return (
-        <Grid.Container align="center" direction="column" style={{minHeight: 'calc(100vh - 280px)'}}>
+        <Grid.Container className="price-section" align="flex-start" direction="column">
             <Spacer y={3}/>
+            {/* <Grid xs justify="center">
+                <Text h3 b>Saves you time and money while creating high guest satisfaction.</Text>
+            </Grid> */}
             <Grid xs justify="center">
-                <Text h1 b>Saves you time and money while creating high guest satisfaction.</Text>
-            </Grid>
-            <Grid xs justify="center">
-                <Tabs title="test">
-                    <Tabs.Body>
-                        <div>Hello world</div></Tabs.Body>
-                </Tabs>
+            <Tabs initialValue="1">
+            <Tabs.Item label="evil rabbit" value="1">The Evil Rabbit Jumped over the Fence.</Tabs.Item>
+            <Tabs.Item label="jumped" value="2">The Fence Jumped over The Evil Rabbit.</Tabs.Item>
+            </Tabs>
             </Grid>
         </Grid.Container>
     )
