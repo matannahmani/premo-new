0b83679da47d274fd144d176bee8db95832f635c
Matan Nahmani <matanmatan999new@gmail.com>
2021-03-30 08:42:43 +0300

    small css fixes for price page

--- pages/price.js
+++ pages/price.js
@@ -6,8 +6,8 @@ const Pricing = () => {
     const { setState, bindings } = useTabs('1')
 
     return (
-        <Grid.Container className="price-section" align="flex-start" direction="column">
-            <Spacer y={3}/>
+        <Grid.Container className="price-section">
+            {/* <Spacer y={3}/> */}
             {/* <Grid xs justify="center">
                 <Text h3 b>Saves you time and money while creating high guest satisfaction.</Text>
             </Grid> */}
